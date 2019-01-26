e988d6665387e161cadaf47ab04c7104461afe90
andrewmcintosh <abmcintosh7@gmail.com>
2019-01-26 12:29:39 -0500

    members are showing and have a link to their show

--- client/src/components/Members/MemberList.js
+++ client/src/components/Members/MemberList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 
 
@@ -8,18 +8,28 @@ import axios from 'axios'
 
 export default class MemberList extends Component {
     state = {
-        users: [{}],
+        members: [{}],
     }
+
+    componentDidMount() {
+        this.getAllMembers()
+    }
+
+
     getAllMembers = () => {
         axios.get(`/api/members`)
-            .then((res) => this.setState({ users: res.data }))
+            .then((res) => this.setState({ members: res.data }))
     }
 
 
     render() {
         return (
             <div>
-                <p>Hi from member list</p>
+                {this.state.members.map((member, i) => (
+                    <div key={i}>
+                        <Link to={`/members/${member._id}`}><h3>{member.name}</h3></Link>
+                    </div>
+                ))}
             </div>
         )
     }
