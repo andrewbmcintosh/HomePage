9a6812a92ce58ce01c6cf25c345439902adab5e7
andrewmcintosh <abmcintosh7@gmail.com>
2019-01-30 10:35:52 -0500

    making Status Ping resemble wireframe

--- client/src/components/Members/MemberList.js
+++ client/src/components/Members/MemberList.js
@@ -35,7 +35,9 @@ export default class MemberList extends Component {
                         <ul>
                             <li>{member.places}</li>
                         </ul>
-                        <StatusPing memberId={member._id} />
+                        <StatusPing memberId={member._id} memberName={member.name} memberCurrentStatus={member.currentStatus} 
+                            memberTimeSincePing={member.timeSincePing}
+                        />
                     </div>
                 ))}
             </div>
