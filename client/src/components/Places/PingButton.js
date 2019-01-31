ca8e4202d34387ba761e6f1fef88b6e868c6b61d
andrewmcintosh <abmcintosh7@gmail.com>
2019-01-31 15:57:15 -0500

    the app is not broken but returning empty array

--- client/src/components/Places/PingButton.js
+++ client/src/components/Places/PingButton.js
@@ -3,14 +3,17 @@ import React, { Component } from 'react'
 export default class PingButton extends Component {
 
     pingLocation = (props) => {
+
         // currentLocation = {this.props.currentLocation}
         // placesData= [this.props.placesData]
         const prevLocation = this.props.placesData.filter(places =>
-        (this.props.placesData.southwestLat < this.props.currentLocation.lat &&
-            this.props.placesData.northeastLat > this.props.currentLocation.lat &&
-            this.props.placesData.southwestLng < this.props.currentLocation.lng &&
-            this.props.placesData.northeastLng > this.props.currentLocation.lng))
+            (this.props.placesData.southwestLat < this.props.currentLocation.lat &&
+                this.props.placesData.northeastLat > this.props.currentLocation.lat &&
+                this.props.placesData.southwestLng < this.props.currentLocation.lng &&
+                this.props.placesData.northeastLng > this.props.currentLocation.lng))
         console.log(prevLocation)
+        console.log(this.props.placesData)
+        console.log(this.props.placesData.southwestLng)
     }
 
     // I should create another then and then a catch that triggers the API and add to database
@@ -21,12 +24,12 @@ export default class PingButton extends Component {
 
 
 
-render() {
-    return (
-        <div>
-            <button onClick={this.pingLocation}>Im the Ping Button</button>
-        </div>
-    )
-}
+    render() {
+        return (
+            <div>
+                <button onClick={this.pingLocation}>Im the Ping Button</button>
+            </div>
+        )
+    }
 }
 
