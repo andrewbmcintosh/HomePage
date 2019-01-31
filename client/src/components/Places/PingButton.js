bc8d4df00096a3bbe1df86ecf5d4ded9db82046f
andrewmcintosh <abmcintosh7@gmail.com>
2019-01-31 17:15:40 -0500

    adding the if statement

--- client/src/components/Places/PingButton.js
+++ client/src/components/Places/PingButton.js
@@ -5,35 +5,26 @@ export default class PingButton extends Component {
         placesData: [{}]
     }
 
-componentDidMount() {
-    this.addPlacesDataToState()
-}
+    componentDidMount() {
+        this.addPlacesDataToState()
+    }
 
 
-addPlacesDataToState = () => {
-    this.setState({placesData: this.props.placesData})
-}
+    addPlacesDataToState = () => {
+        this.setState({ placesData: this.props.placesData })
+    }
 
     pingLocation = (props) => {
-
-        // currentLocation = {this.props.currentLocation}
-        // placesData= [this.props.placesData]
         const prevLocation = this.props.placesData.filter(places =>
             (places.southwestLat < this.props.currentLocation.lat &&
                 places.northeastLat > this.props.currentLocation.lat &&
                 places.southwestLng < this.props.currentLocation.lng &&
-                places.northeastLng > this.props.currentLocation.lng))
-        // const prevLocation = this.props.placesData.filter(places =>
-        //     (this.props.placesData.southwestLat < this.props.currentLocation.lat &&
-        //         this.props.placesData.northeastLat > this.props.currentLocation.lat &&
-        //         this.props.placesData.southwestLng < this.props.currentLocation.lng &&
-        //         this.props.placesData.northeastLng > this.props.currentLocation.lng))
+                places.northeastLng > this.props.currentLocation.lng));
+
+
         console.log(prevLocation)
-        // console.log(this.props.placesData)
-        console.log(this.props.placesData.southwestLng)
     }
 
-    // I should create another then and then a catch that triggers the API and add to database
 
 
 
