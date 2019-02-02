42540622c1d472a47b8b33e25515696bdd694ab3
andrewmcintosh <abmcintosh7@gmail.com>
2019-02-02 17:25:38 -0500

    installing the new material

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import LandingPage from './components/LandingPage'
 import MemberList from './components/Members/MemberList';
--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react'
 import MemberList from './Members/MemberList';
+// import { createMuiTheme } from '@material-ui/core/styles';
+
+// import PropTypes from 'prop-types';
+// import classNames from 'classnames';
+
+// const theme = createMuiTheme({
+
+// })
+
+const styles = theme => ({
+    layout: {
+        width: 'auto',
+        marginLeft: theme.spacing.unit * 3,
+        marginRight: theme.spacing.unit * 3,
+        [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
+            width: 1100,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        }
+    }
+
+})
 
 
 export default class LandingPage extends Component {
 
     render() {
+        const { classes } = this.props;
+
         return (
-         <div>
-         <MemberList />
-         </div>
-            
+            <div className={classes.root}>
+                <MemberList />
+            </div>
+
         );
     }
 }
