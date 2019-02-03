8c621dc5745e93515621a53888845f550d696666
andrewmcintosh <abmcintosh7@gmail.com>
2019-02-03 10:41:59 -0500

    still learning material-ui

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,36 +1,18 @@
 import React, { Component } from 'react'
 import MemberList from './Members/MemberList';
-// import { createMuiTheme } from '@material-ui/core/styles';
+// import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/styles';
+import Button from '@material-ui/core/Button';
 
-// import PropTypes from 'prop-types';
-// import classNames from 'classnames';
 
-// const theme = createMuiTheme({
-
-// })
-
-const styles = theme => ({
-    layout: {
-        width: 'auto',
-        marginLeft: theme.spacing.unit * 3,
-        marginRight: theme.spacing.unit * 3,
-        [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
-            width: 1100,
-            marginLeft: 'auto',
-            marginRight: 'auto',
-        }
-    }
-
-})
 
 
 export default class LandingPage extends Component {
 
     render() {
-        const { classes } = this.props;
 
         return (
-            <div className={classes.root}>
+            <div >
                 <MemberList />
             </div>
 
