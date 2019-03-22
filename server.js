642412e6b5cd1051b08ccaf9e7b5a6695ebaa981
andrewmcintosh <abmcintosh7@gmail.com>
2019-03-22 16:49:51 -0400

    addressing 503 heroku error

--- server.js
+++ server.js
@@ -1,7 +1,3 @@
-var cors = require('cors');
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 const express = require('express');
 require('dotenv').config();
 const app = express();
@@ -17,7 +13,6 @@ app.get('/', (req, res) => {
 });
 
 app.use('/', routes);
-mongoose.connect(process.env.MONGODB_URI);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
